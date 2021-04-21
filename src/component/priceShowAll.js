5115755bd344b15e7d249414f9259af67d4a3493
Bas10699 <bas10699@gmail.com>
2021-04-21 20:26:28 +0700

    เพิ่มแสดง2ตัวบน-ล่าง

--- src/component/priceShowAll.js
+++ src/component/priceShowAll.js
@@ -4,7 +4,8 @@ import firebase, { db } from '../firebase'
 
 const PriceShowAll = () => {
     const [showData, setShowData] = useState([])
-    const [show200, setShow200] = useState([])
+    const [showTop200, setShowTop200] = useState([])
+    const [showDown200, setShowDown200] = useState([])
     useEffect(() => {
 
         db.collection("lotto").onSnapshot((querySnapshot) => {
@@ -28,18 +29,29 @@ const PriceShowAll = () => {
 
             });
 
-            let lotto200 = []
+            let lottoTop200 = []
+            let lottoDown200 = []
             lotto.map((eleLotto) => {
-                let sum = 0
+                let sumTop = 0
+                let sumDown = 0
                 shData.map((eleData) => {
-                    if (eleLotto === eleData.numLotto) {
-                        sum += eleData.priceLotto
+                    if ((eleLotto === eleData.numLotto) && (eleData.typeLotto === 0)) {
+                        sumTop += eleData.priceLotto
+                    }
+                    else if ((eleLotto === eleData.numLotto) && (eleData.typeLotto === 1)) {
+                        sumDown += eleData.priceLotto
                     }
                 })
-                if (sum > 200) {
-                    lotto200.push({
+                if (sumTop >= 200) {
+                    lottoTop200.push({
                         numLotto: eleLotto,
-                        sumPrice: sum
+                        sumPrice: sumTop
+                    })
+                }
+                if (sumDown >= 200) {
+                    lottoDown200.push({
+                        numLotto: eleLotto,
+                        sumPrice: sumDown
                     })
                 }
 
@@ -48,7 +60,8 @@ const PriceShowAll = () => {
             // console.log("lotto", lotto200)
             // console.log("Data", shData)
             setShowData(shData)
-            setShow200(lotto200)
+            setShowTop200(lottoTop200)
+            setShowDown200(lottoDown200)
 
         });
 
@@ -57,17 +70,42 @@ const PriceShowAll = () => {
     return (
         <div className="row">
             <div className="col-sm-6">
-                <h6>รายการตัวเลขราคารวมเกิน 200 บาท</h6>
+                {/* <h6>รายการตัวเลขราคารวมเกิน 200 บาท</h6> */}
                 <table className="table table-sm">
-                    <thead>
+                    <thead className="thead-dark">
                         <tr>
                             <th scope="col">#</th>
-                            <th scope="col">ตัวเลข</th>
+                            <th scope="col">2ตัวบน</th>
                             <th scope="col">ราคา</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {show200.map((element, index) => {
+                        {showTop200.map((element, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{element.numLotto}</td>
+                                    <td>{element.sumPrice}</td>
+                                </tr>
+                            )
+                        })}
+
+
+                    </tbody>
+                </table>
+            </div>
+            <div className="col-sm-6">
+                {/* <h6>รายการตัวเลขราคารวมเกิน 200 บาท</h6> */}
+                <table className="table table-sm">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">2ตัวล่าง</th>
+                            <th scope="col">ราคา</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {showDown200.map((element, index) => {
                             return (
                                 <tr key={index}>
                                     <td>{index + 1}</td>
@@ -82,7 +120,7 @@ const PriceShowAll = () => {
                 </table>
             </div>
 
-            <div className="col-sm-6">
+            {/* <div className="col-sm-6">
                 <h6>รายการตัวเลขทั้งหมด</h6>
                 <table className="table table-sm">
                     <thead>
@@ -108,7 +146,7 @@ const PriceShowAll = () => {
 
                     </tbody>
                 </table>
-            </div>
+            </div> */}
 
 
 
